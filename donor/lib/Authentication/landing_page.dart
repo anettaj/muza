954e4c4e0f16a1b8e5240297aee70e516b2c3dc1
Anet Taj <58222128+anettaj@users.noreply.github.com>
2023-08-20 14:05:22 +0530

    ADD

---
+++ donor/lib/Authentication/landing_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Landing_page extends StatelessWidget {
+  const Landing_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("hello"),
+    );
+  }
+}
