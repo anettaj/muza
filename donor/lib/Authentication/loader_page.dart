767ff07cb5b8703e90f5d446fb0459ee16e57578
Anet Taj <58222128+anettaj@users.noreply.github.com>
2023-08-27 14:05:28 +0530

    add

--- donor/lib/Authentication/loader_page.dart
+++ donor/lib/Authentication/loader_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:scaffold_gradient_background/scaffold_gradient_background.dart';
-import 'package:donor/Authentication/Components/sizing_components.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-class Loader extends StatelessWidget {
+import 'dart:async';
+import 'package:donor/Authentication/Components/sizing_components.dart';
+import 'landing_page.dart'; // Import your LandingPage
+
+class Loader extends StatefulWidget {
+  @override
+  _LoaderState createState() => _LoaderState();
+}
+
+class _LoaderState extends State<Loader> {
+  @override
+  void initState() {
+    super.initState();
+    // Start the timer in the initState method
+    Timer(
+      const Duration(seconds: 3),
+          () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LandingPage(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Sizing sizing = Sizing(context);
-    // double screenWidth = sizing.setWidth();
     double screenHeight = sizing.setHeight();
     return ScaffoldGradientBackground(
       gradient: const LinearGradient(
@@ -18,14 +42,20 @@ class Loader extends StatelessWidget {
           Color(0xFF030303),
         ],
       ),
-      body:Container(
+      body: Container(
         child: Center(
           child: SvgPicture.asset(
             'lib/Authentication/Assets/icon.svg',
-              height: screenHeight*0.2,
+            height: screenHeight * 0.2,
           ),
         ),
       ),
     );
+  }
+}
 
-  }}
+void main() {
+  runApp(MaterialApp(
+    home: Loader(), // Use your splash screen widget as the initial route
+  ));
+}
